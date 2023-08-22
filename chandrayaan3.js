bacb1dfc7ceb4846015f861e847ae65414c1063e
Rishabh Rai <mailmerairishabh99@gmail.com>
2023-08-22 18:07:00 +0530

    Recommit: Execute command successfully

--- chandrayaan3.js
+++ chandrayaan3.js
@@ -7,6 +7,35 @@ const indexMove = {
 
 const compass = ["N", "E", "S", "W"]
 
+const executeCommand = (position, direction, command) => {
+    switch (command) {
+        case "f":
+            return executeF(position, direction);
+        case "b":
+            return executeB(position, direction);
+        case "u":
+            return executeU(position, direction);
+        case "d":
+            return executeD(position, direction);
+        case "l":
+            return executeL(position, direction);
+        case "r":
+            return executeR(position, direction);
+    }
+}
+
+const executeCommands = (position, direction, commands) => {
+    let currentPosition = position
+    let currentDirection = direction
+    commands.forEach((command) => {
+        const newState = executeCommand(currentPosition, currentDirection, command)
+        currentPosition = newState[0]
+        currentDirection = newState[1]
+
+    })
+    return [currentPosition, currentDirection]
+}
+
 const executeF = (position, direction) => {
     const change = positiveDirections.includes(direction) ? 1 : -1;
     return move(position, direction, change);
@@ -15,13 +44,6 @@ const executeF = (position, direction) => {
 const executeB = (position, direction) => {
     const change = positiveDirections.includes(direction) ? -1 : 1;
     return move(position, direction, change);
-};
-
-const move = (position, direction, change) => {
-    const newPosition = [...position];
-    const [index] = Object.entries(indexMove).find(([i, directions]) => directions.includes(direction));
-    newPosition[index] = newPosition[index] + change;
-    return [newPosition, direction];
 };
 
 const executeU = (position, direction) => {
@@ -34,7 +56,9 @@ const executeD = (position, direction) => {
 
 const executeL = (position, direction) => {
     const index = ["U", "D"].includes(direction) ? 0 : compass.indexOf(direction)
+    console.log("index>>>", index)
     const leftIndex = index == 0 ? compass.length - 1 : index - 1
+    console.log("elft>>>", leftIndex)
     return [position, compass[leftIndex]]
 };
 
@@ -44,11 +68,20 @@ const executeR = (position, direction) => {
     return [position, compass[rightIndex]]
 };
 
+const move = (position, direction, change) => {
+    const newPosition = [...position];
+    const [index] = Object.entries(indexMove).find(([i, directions]) => directions.includes(direction));
+    newPosition[index] = newPosition[index] + change;
+    return [newPosition, direction];
+};
+
 module.exports = {
     executeF,
     executeB,
-    executeU,
     executeD,
+    executeR,
     executeL,
-    executeR
+    executeU,
+    executeCommands,
+    executeCommand
 };
--- test.spec.js
+++ test.spec.js
@@ -1,4 +1,4 @@
-const { executeF, executeB, executeU, executeD, executeL, executeR } = require("./chandrayaan3")
+const { executeF, executeB, executeR, executeL, executeU, executeD, executeCommand, executeCommands } = require("./chandrayaan3")
 
 test("execute command f successfully", () => {
     const [position, direction] = executeF([0, 0, 0], "N")
@@ -12,6 +12,17 @@ test("execute command b successfully", () => {
     expect(direction).toEqual("D")
 })
 
+test("execute command r successfully", () => {
+    const [position, direction] = executeR([0, 7, 8], "S")
+    expect(position).toEqual([0, 7, 8])
+    expect(direction).toEqual("W")
+})
+
+test("execute command l successfully", () => {
+    const [position, direction] = executeL([0, 1, 0], "U")
+    expect(position).toEqual([0, 1, 0])
+    expect(direction).toEqual("W")
+})
 test("execute command u successfully", () => {
     const [position, direction] = executeU([0, 0, 0], "N")
     expect(position).toEqual([0, 0, 0])
@@ -24,14 +35,35 @@ test("execute command d successfully", () => {
     expect(direction).toEqual("D")
 })
 
-test("execute command r successfully", () => {
-    const [position, direction] = executeR([0, 7, 8], "S")
-    expect(position).toEqual([0, 7, 8])
-    expect(direction).toEqual("W")
+test("execute command successfully", () => {
+
+    let [p1, d1] = executeCommand([0, 0, 0], "N", "f")
+    expect(p1).toEqual([0, 1, 0])
+    expect(d1).toEqual("N")
+
+    let [p2, d2] = executeCommand([0, -4, 0], "D", "b")
+    expect(p2).toEqual([0, -4, 1])
+    expect(d2).toEqual("D")
+
+    let [p3, d3] = executeCommand([0, 7, 8], "S", "r")
+    expect(p3).toEqual([0, 7, 8])
+    expect(d3).toEqual("W")
+
+    let [p4, d4] = executeCommand([0, 1, 0], "U", "l")
+    expect(p4).toEqual([0, 1, 0])
+    expect(d4).toEqual("W")
+
+    let [p5, d5] = executeCommand([0, 0, 0], "N", "u")
+    expect(p5).toEqual([0, 0, 0])
+    expect(d5).toEqual("U")
+
+    let [p6, d6] = executeCommand([0, 0, 0], "N", "d")
+    expect(p6).toEqual([0, 0, 0])
+    expect(d6).toEqual("D")
 })
 
-test("execute command l successfully", () => {
-    const [position, direction] = executeL([0, 1, 0], "U")
-    expect(position).toEqual([0, 1, 0])
+test("execute commands successfully", () => {
+    const [position, direction] = executeCommands([0, 0, 0], "N", ["f", "r", "u", "b", "l"])
+    expect(position).toEqual([0, 1, -1])
     expect(direction).toEqual("W")
 })
